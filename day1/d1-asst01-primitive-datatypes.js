1465d60fcb41d0e17f97991a2ba63229e9a16ba3
H Meethal <71983829+hmeethal30@users.noreply.github.com>
2025-04-11 12:17:21 +0100

    Day1 assignments initial commit

---
+++ day1/d1-asst01-primitive-datatypes.js
@@ -0,0 +1,22 @@
+/* 
+Assignment Requirements:  
+    Create the following variables using let (not using var) and check their typeOf 
+    a) firstName 
+    b) companyName 
+    c) mobileNumber 
+    d) isAutomation 
+    e) hasPlaywright (do not assign) 
+ Print and confirm the values and data types 
+*/
+
+let firstName = "Orange";
+let companyName = "Apple";
+let mobileNumber = 1234567890;
+let isAutomation = true;
+let hasPlaywright;
+
+console.log("First Name: " + firstName + " is of type " + typeof firstName);
+console.log("Company Name: " + companyName + " is of type " + typeof companyName);
+console.log("Mobile Number: " + mobileNumber + " is of type " + typeof mobileNumber);
+console.log("Is Automation: " + isAutomation + " is of type " + typeof isAutomation);   
+console.log("Has Playwright: " + hasPlaywright + " is of type " + typeof hasPlaywright);
